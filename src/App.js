9c54ccbf7990aecec64bfd8026fa75dc0ce00bd4
manaemee <78135473+manaemee@users.noreply.github.com>
2022-01-23 22:28:45 +0900

    to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import styles from "./App.module.css";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState(localStorage.getItem("TODOS") ? JSON.parse(localStorage.getItem("TODOS")): []);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+    const onClick = (event) =>{
+     setToDos( toDos.filter((toDo) => event.target.id !== toDo));
+    }
+    localStorage.setItem("TODOS", JSON.stringify(toDos));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.todobox}>
+      <h1 className={styles.title}>To Do List</h1>
+      <form onSubmit={onSubmit}>
+        <input className={styles.insert} onChange={onChange}  value={toDo} type="text" maxlength="57" placeholder="Write your to do..."/>
+        <button className={styles.formbutton}>ADD</button>
+      </form>
+      <hr/>
+  <div className={styles.content}>
+    {toDos.map((todo, index)=>
+    <div className={styles.todo} key={index}>
+      <span id={todo}>{todo}</span>
+      <span id={todo} onClick={onClick} className={styles.button}>🗑</span>
     </div>
+    )}
+  </div>
+     </div>
   );
 }
+
 
 export default App;
